a20c5790c917b69e35c5ec71a0bcdb7055faaa39
Song Han <songhan@stanford.edu>
2014-05-10 20:56:52 -0700

    added debug.h

---
+++ replFs/debug.h
@@ -0,0 +1,56 @@
+/*
+ * debug.h
+ *
+ *  Created on: May 10, 2014
+ *      Author: songhan
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#define DEBUG_ON
+
+#define ERROR(...)
+do {
+  fprintf(stderr, "[ERROR  ]%s %s(Line %d): ",__FILE__,__FUNCTION__,__LINE__);
+  fprintf(stderr, __VA_ARGS__);
+}while(0)
+
+#define WARNING(...)
+do {
+  fprintf(stdout, "[WARNING]%s %s(Line %d): ",__FILE__,__FUNCTION__,__LINE__);
+  fprintf(stdout, __VA_ARGS__);
+}while(0)
+
+#define INFO(...)
+do {
+  fprintf(stdout, "[INFO  ]%s %s(Line %d): ",__FILE__,__FUNCTION__,__LINE__);
+  fprintf(stdout, __VA_ARGS__);
+}while(0)
+
+#define SHOW_TIME(...)
+do {
+  extern unsigned long long gLatestTime;
+  timeval tp;
+  gettimeofday(&tp, NULL);
+  unsigned long long now = tp.tv_sec*1000000+tp.tv_usec;
+  if(gLatestTime != 0)
+  {
+    fprintf(stdout, ">>>>>>>>>Used Time: %s[%d], %s: %ld.%ld, %llu ms ", __FILE__, __LINE__, __func__, tp.tv_sec, tp.tv_usec, (now-gLatestTime)1000);
+    fprintf(stdout, __VA_ARGS__);
+    fprintf(stdout, "n");
+  }
+  gLatestTime = now;
+}while(0)
+
+#ifdef DEBUG_ON
+#define DEBUG(...)
+do {
+  fprintf(stdout, "[DEBUG  ]%s %s(Line %d): ",__FILE__,__FUNCTION__,__LINE__);
+  fprintf(stdout, __VA_ARGS__);
+}while(0)
+#else
+#define DEBUG(...)
+#endif
+
+#endif /* DEBUG_H_ */
